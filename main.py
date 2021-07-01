a6c724a18abb9ad023864cf9b50ba60045804f9f
mehul208 <mehul.r2001@gmail.com>
2021-07-01 23:11:43 +0530

    meet code input and join

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox()
 driver.get("https://meet.google.com/")
+driver.implicitly_wait(4)
+meet_code = "gwh-yaxb-nka"
 
-time.sleep(10)
-driver.close()
+try:
+    meet_code_input = driver.find_elements_by_css_selector('input.glue-caption')[1]
+    meet_code_input.send_keys(meet_code)
+    button = driver.find_element_by_css_selector('button.join-button')
+    button.click()
+
+except NoSuchElementException:
+    print("Not Found")
+
+# driver.close()
